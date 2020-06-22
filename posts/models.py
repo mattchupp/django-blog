dcc57acdc44211b910711b2a0c24bbe27fa53ce8
mattchupp <mattlchupp@gmail.com>
2020-06-22 09:32:27 -0500

    new model for posts

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model): 
+  post_title = models.CharField(max_length=200)
+  post_content = models.TextField(max_length=None, serialize=True)
