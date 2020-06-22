5382a44494e76aa39e80ab3353da82f36cb07691
mattchupp <mattlchupp@gmail.com>
2020-06-22 11:36:06 -0500

    render view from templates with list of posts

--- posts/views.py
+++ posts/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Post
 
 # Create your views here.
 def index(request): 
-  return HttpResponse('Hello from posts!')
+
+  # get list of posts and sort by date
+  latest_posts = Post.objects.order_by('post_date')
+
+  # context for latest_posts in template
+  context = {
+    'latest_posts': latest_posts,
+  }
+
+  # render the request with template polls/index.html 
+  # with context of latest posts
+  return render(request, 'posts/index.html', context)
+
 
 def post(request, post_id): 
   response = "Here is the page for %s."
