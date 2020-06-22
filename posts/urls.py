0f83931450108054af2ed093f3b557df01b0366e
mattchupp <mattlchupp@gmail.com>
2020-06-22 11:24:04 -0500

    set up path for individual post

--- posts/urls.py
+++ posts/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('', views.index, name='index')
+  # path -> /posts
+  path('', views.index, name='index'),
+  # path /post/1
+  path('<int:post_id>/', views.post, name='post')
 ]
