031ea5161d93c361bf6bb83036a6e6015e7fd241
mattchupp <mattlchupp@gmail.com>
2020-06-22 11:04:01 -0500

    added datetimefile

--- posts/models.py
+++ posts/models.py
@@ -4,3 +4,4 @@ from django.db import models
 class Post(models.Model): 
   post_title = models.CharField(max_length=200)
   post_content = models.TextField(max_length=None, serialize=True)
+  post_date = models.DateTimeField(auto_now=True)
