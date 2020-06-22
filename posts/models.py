3e814e379a30ea459e2b92146fb49a30bbd873f4
mattchupp <mattlchupp@gmail.com>
2020-06-22 11:11:19 -0500

    documentation on posts model

--- posts/models.py
+++ posts/models.py
@@ -1,7 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+# Model for Posts
 class Post(models.Model): 
   post_title = models.CharField(max_length=200)
   post_content = models.TextField(max_length=None, serialize=True)
   post_date = models.DateTimeField(auto_now=True)
+
+  # Set a nicer title for posts in admin console 
+  def __str__(self): 
+    return self.post_title
