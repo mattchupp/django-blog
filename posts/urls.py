e7629f17bcbd1234c353019ec89d2830ec33d6c8
mattchupp <mattlchupp@gmail.com>
2020-06-22 15:49:53 -0500

    namespacing urls and removing hard code for them on posts page

--- posts/urls.py
+++ posts/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from . import views
 
+# giving posts an app name to have namespaced urls in templates
+app_name = 'posts'
+
 urlpatterns = [
   # path -> /posts
   path('', views.index, name='index'),
-  # path /post/1
+  # path -> /post/1/
   path('<int:post_id>/', views.post, name='post')
 ]
