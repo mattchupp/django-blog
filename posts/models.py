5d0b62b7f6be98e13950cdc25bdafeeae7b06ec8
mattchupp <mattlchupp@gmail.com>
2020-07-04 22:04:47 -0500

    added a rich editor field - it doesnt work right

--- posts/models.py
+++ posts/models.py
@@ -1,11 +1,13 @@
 from django.db import models
+from martor.models import MartorField
 
 # Create your models here.
 
 # Model for Posts
 class Post(models.Model): 
   post_title = models.CharField(max_length=200)
-  post_content = models.TextField(max_length=None, serialize=True)
+  # post_content = models.TextField(max_length=None, serialize=True)
+  post_content = MartorField()
   post_date = models.DateTimeField(auto_now=True)
 
   # Set a nicer title for posts in admin console 
--- posts/urls.py
+++ posts/urls.py
@@ -1,10 +1,12 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 # giving posts an app name to have namespaced urls in templates
 app_name = 'posts'
 
 urlpatterns = [
+  # martor app
+  path('martor/', include('martor.urls')),
   # path -> /posts
   path('', views.index, name='index'),
   # path -> /post/1/
